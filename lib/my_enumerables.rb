23382d7779bf87cee41cb0a7c2b7a66abd16d6d7
Dylan Maloney <maloney.dylan@gmail.com>
2022-02-15 15:42:12 -0500

    Refacfor and test my_inject

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,11 +1,78 @@
 module Enumerable
-  # Your code goes here
-end
+  def my_each
+    i = 0
+    while i < length
+      yield self[i] unless is_a?(Hash)
+      yield [keys[i], values[i]] if is_a?(Hash)
+      i += 1
+    end
+    self
+  end
 
-# You will first have to define my_each
-# on the Array class. Methods defined in
-# your enumerable module will have access
-# to this method
-class Array
-  # Define my_each here
+  def my_each_with_index
+    i = 0
+    while i < length
+      yield self[i], i unless is_a?(Hash)
+      yield [keys[i], values[i], i] if is_a?(Hash)
+      i += 1
+    end
+    self
+  end
+
+  def my_select
+    result = []
+    my_each { |x| result << x if yield(x) }
+    result
+  end
+
+  def my_all?
+    result = []
+    my_each { |x| result << x if yield(x) }
+    result == self
+  end
+
+  def my_any?
+    result = []
+    my_each { |x| result << x if yield(x) }
+    if result.empty?
+      false
+    else
+      true
+    end
+  end
+
+  def my_none?
+    result = []
+    my_each { |x| result << x if yield(x)}
+    return result.empty?
+  end
+
+  def my_count
+    result = []
+    if block_given?
+      my_each { |x| result << x if yield(x)}
+      return result.length
+    else
+      return self.length
+    end
+  end
+
+  def my_map
+    result = []
+    if block_given?
+      my_each { |x| result << yield(x)}
+      result
+    else
+      self
+    end
+  end
+
+  def my_inject
+    i = 0
+    while i < length - 1
+      result = yield(result, self[i = 1])
+      i += 1
+    end
+    return result
+  end
 end
